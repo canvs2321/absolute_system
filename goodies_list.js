27ffe2bdc50ee134ae89744603d504ed351f2a7b
canvs2321 <canvs2321@gmail.com>
2010-07-19 15:09:10 -0400

    goodies_list.js

--- goodies_list.js
+++ goodies_list.js
@@ -1,3 +1,5 @@
 {
-	jrummymirror1:"http://www.froyoroms.com/files/developers/jrummy/JRummy/Other/goodies.zip"
+	jrummymirror1:"http://www.froyoroms.com/files/developers/jrummy/JRummy/Other/goodies.zip",
+	jrummymirror2:"http://www.froyoroms.com/files/developers/jrummy/JRummy/Other/goodies.zip",
+	jrummymirror3:"http://www.froyoroms.com/files/developers/jrummy/JRummy/Other/goodies.zip"
 }
